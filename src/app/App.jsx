7053547ae8e3da770588410c11dbd28434d747ca
Frans Jude Castillo <fransjude@email.com>
2026-05-16 13:25:27 -0600

    Final version of Everyday Market App

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/common/Header";
+import ProductsPage from "./components/market/ProductsPage";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ProductsPage />
+    </>
+  );
+}
+
+export default App;
